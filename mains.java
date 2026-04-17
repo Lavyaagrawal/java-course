f73a466a84440e4561dd83fc6302d7df8f3a32cf
Lavya Agrawal <lavyaagrawal123@gmail.com>
2026-04-17 10:59:04 +0530

    taking user input

---
+++ mains.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class mains{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String fname=sc.nextLine();
+        System.out.println("My name is"+ fname);
+        sc.close();
+
+    }
+}
