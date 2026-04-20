1b0633b6821a134f09b3b1f79d8bf0d33c40dd0d
Lavya Agrawal <lavyaagrawal123@gmail.com>
2026-04-20 09:44:27 +0530

    even array

---
+++ even.java
@@ -0,0 +1,25 @@
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
+import java.util.Scanner;
+
+public class even {
+   
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      System.out.print("enter the size of array: ");
+      int n= sc.nextInt();
+      int []arr= new int [n];
+      System.out.print("enter thr element");
+      for(int i=0;i<n;i++){
+         arr[i]=sc.nextInt();
+      }
+      System.out.println("Even numbers are:");
+      for(int i =0;i<n;i++){
+         if(arr[i]%2==0){
+            System.out.print(arr[i]+" ");
+
+
+         }
+      }
+   sc.close();
+}
+}
