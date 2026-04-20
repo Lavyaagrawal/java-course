dcccb28587cc1277480c97026bc1945d45fbe2c4
Lavya Agrawal <lavyaagrawal123@gmail.com>
2026-04-20 09:51:03 +0530

    added counter variable

--- even.java
+++ even.java
@@ -5,6 +5,7 @@ public class even {
    
    public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
+      
       System.out.print("enter the size of array: ");
       int n= sc.nextInt();
       int []arr= new int [n];
@@ -12,14 +13,17 @@ public class even {
       for(int i=0;i<n;i++){
          arr[i]=sc.nextInt();
       }
+      int count =0;
       System.out.println("Even numbers are:");
       for(int i =0;i<n;i++){
          if(arr[i]%2==0){
             System.out.print(arr[i]+" ");
+            count++;
 
 
          }
       }
+      System.out.println("\nTotal even numbers: " + count);
    sc.close();
 }
 }
